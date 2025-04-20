786271e970b07d1ccaf71c642b86e69efb932458
Ayushrpatil <patil.ay@northeastern.edu>
2025-04-20 16:26:17 -0400

    added the documentation to 2048 game

--- src/mcts/game2048/Game2048.java
+++ src/mcts/game2048/Game2048.java
@@ -1,1 +1,16 @@
+package com.phasmidsoftware.dsaipg.projects.mcts.game2048;
 
+import com.phasmidsoftware.dsaipg.projects.mcts.core.Game;
+import com.phasmidsoftware.dsaipg.projects.mcts.core.State;
+
+public class Game2048 implements Game<Game2048> {
+    @Override
+    public State<Game2048> start() {
+        return new State2048();
+    }
+
+    @Override
+    public int opener() {
+        return 0; // Only one player
+    }
+}
